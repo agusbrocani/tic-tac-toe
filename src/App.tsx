2df26acff023e3611ca9e0040c9024f1bc816064
agusbrocani <aguss.brocani@gmail.com>
2024-11-23 22:56:42 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Cuadrado from './cuadrado'
+
+/*
+<></> => forma proporcionada por React para encapsular varios elementos 
+sin añadir un elemento HTML extra al DOM
+*/
+function App() {
+  return (
+    <>
+        <Cuadrado/>
+    </>
+  )
+}
+
+export default App
