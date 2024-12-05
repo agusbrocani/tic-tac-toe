1a7141c74b129c28c55597f18cc4c62eb4550760
agusbrocani <aguss.brocani@gmail.com>
2024-12-05 13:52:52 -0300

    Comentarios sobre las mejoras del codigo anteriores

--- src/Tablero.tsx
+++ src/Tablero.tsx
@@ -2,34 +2,34 @@ import Boton from './Boton'
 
 function Tablero(): JSX.Element
 {
+  const cantidadDeBotones = 9
+  const cantidadDeFilas = 3
+  const botones = [];
+  const filas = [];
+
   function handleClick(evento: React.MouseEvent<HTMLButtonElement, MouseEvent>): void
   {
     console.log(evento.currentTarget.id)
     console.log(evento.currentTarget.value)
   }
 
-  const cantidadDeBotones = 9
-  const cantidadDeFilas = 3
-
-  const botones = [];
   for(let i = 0; i < cantidadDeBotones; i++) 
   {
     botones.push(
       <Boton
-        key = {i}
-        id = {`${i}`}
+        key = {i} //Identificador para componentes en el árbol -> Reconciliación.
+        id = {`${i}`} //Identificar cada elemento del DOM
         value = {(i).toString()}  //value = ""
         handleClick = {handleClick}
       />
     )
   }
 
-  const filas = [];
   for (let i = 0; i < cantidadDeFilas; i++) 
   {
     filas.push(
       <div key={i}>
-        {botones.slice(i * 3, (i + 1) * 3)}
+        {botones.slice(i * 3, (i + 1) * 3) /* shallow copy[copia referencia -> son componentes]: desde index 0, 3 y 6 hasta 3, 6 y 9 [sin incluir]*/}
       </div>
     )
   }
