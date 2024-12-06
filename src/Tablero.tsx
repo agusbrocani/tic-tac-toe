c9dce8c04ff2332119e1b94dac7aad7c69665f43
agusbrocani <aguss.brocani@gmail.com>
2024-12-06 09:50:52 -0300

    Agrego array de ganadores y array de X y O

--- src/Tablero.tsx
+++ src/Tablero.tsx
@@ -3,10 +3,24 @@ import { useState } from 'react'
 
 function Tablero(): JSX.Element
 {
+  const combinacionesGanadoras: number[][] = 
+  [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 4, 6],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8]
+  ]
   const cantidadDeBotones = 9
   const cantidadDeFilas = 3
   const botones = []
   const filas = []
+  const cantidadMaximaDeCasillasOcupadas = 5
+  const casillasOcupadasX: number[] = Array(cantidadMaximaDeCasillasOcupadas).fill(-1)
+  const casillasOcupadasO: number[] = Array(cantidadMaximaDeCasillasOcupadas - 1).fill(-1)
 
   const [nroTurno, setNroTurno] = useState(0)
 
@@ -14,6 +28,11 @@ function Tablero(): JSX.Element
   {
     // console.log(evento.currentTarget.id)
     // console.log(evento.currentTarget.textContent)
+
+    combinacionesGanadoras.map((valor: number[], index: number) => {
+      console.log('Valor:', valor)      
+      console.log('Índice:', index) 
+    })
 
     if(0 === nroTurno % 2) //el turno es par => X
     {
