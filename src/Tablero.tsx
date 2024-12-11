0e18f5e5cbf7daf79881ec0e62fe08af7171def6
agusbrocani <aguss.brocani@gmail.com>
2024-12-11 12:08:54 -0300

    Correccion de juego: funciona correctamente

--- src/Tablero.tsx
+++ src/Tablero.tsx
@@ -2,9 +2,8 @@ import Boton from './Boton'
 import { useState } from 'react'
 import { insertarOrdenado } from './utils/insertarOrdenado'
 
-function verificarGanador(coleccionAVerificar: number[]): boolean
-  {
-    const combinacionesGanadoras: number[][] = 
+function verificarGanador(coleccionAVerificar: number[]): boolean {
+  const combinacionesGanadoras: number[][] =
     [
       [0, 1, 2],
       [0, 3, 6],
@@ -15,41 +14,35 @@ function verificarGanador(coleccionAVerificar: number[]): boolean
       [3, 4, 5],
       [6, 7, 8]
     ]
-    const ceColeccionAVerificar = coleccionAVerificar.length
-    const ceCombinacionesGanadoras = combinacionesGanadoras[0].length
-    
-    if(ceColeccionAVerificar < ceCombinacionesGanadoras) //salida anticipada en caso de que no haya que verificar
-    {
-      return false
-    }
+  const ceColeccionAVerificar = coleccionAVerificar.length
+  const ceCombinacionesGanadoras = combinacionesGanadoras[0].length
 
-    for(const combinacionGanadora of combinacionesGanadoras)
-    {
-      let idxColeccionAVerificar = 0
-      let match = 0
-      while(idxColeccionAVerificar < ceColeccionAVerificar)
-      {
-        for(let casilla = 0; casilla < ceCombinacionesGanadoras; casilla++)
-        {
-          if(combinacionGanadora[casilla] === coleccionAVerificar[idxColeccionAVerificar])
-          {
-            match++
-            break
-          }
-        }
-        if(ceCombinacionesGanadoras === match)
-        {
-          return true
-        }
-        idxColeccionAVerificar++
-      }
-    }
-
+  if (ceColeccionAVerificar < ceCombinacionesGanadoras) //salida anticipada en caso de que no haya que verificar
+  {
     return false
   }
 
-function Tablero(): JSX.Element
-{
+  for (const combinacionGanadora of combinacionesGanadoras) {
+    let idxColeccionAVerificar = 0
+    let match = 0
+    while (idxColeccionAVerificar < ceColeccionAVerificar) {
+      for (let casilla = 0; casilla < ceCombinacionesGanadoras; casilla++) {
+        if (combinacionGanadora[casilla] === coleccionAVerificar[idxColeccionAVerificar]) {
+          match++
+          break
+        }
+      }
+      if (ceCombinacionesGanadoras === match) {
+        return true
+      }
+      idxColeccionAVerificar++
+    }
+  }
+
+  return false
+}
+
+function Tablero(): JSX.Element {
   const botones = []
   const filas = []
   const cantidadDeFilas = 3
@@ -65,63 +58,61 @@ function Tablero(): JSX.Element
 
   const [nroTurno, setNroTurno] = useState(0)
 
-  function handleClick(evento: React.MouseEvent<HTMLButtonElement, MouseEvent>): void
-  {
+  function handleClick(evento: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
     // console.log(evento.currentTarget.id)
     // console.log(evento.currentTarget.textContent)
 
-    if(0 === nroTurno % 2) //el turno es par => X
+    if (0 === nroTurno % 2) //el turno es par => X
     {
       evento.currentTarget.textContent = "X"
       casillasOcupadas = [...casillasOcupadasX]
       casillasOcupadas = insertarOrdenado(casillasOcupadas, Number(evento.currentTarget.id), comparaEnteros)
       setCasillasOcupadasX(casillasOcupadas)
     }
-    else
-    {
+    else {
       evento.currentTarget.textContent = "O"
       casillasOcupadas = [...casillasOcupadasO]
       casillasOcupadas = insertarOrdenado(casillasOcupadas, Number(evento.currentTarget.id), comparaEnteros)
       setCasillasOcupadasO(casillasOcupadas)
     }
+    
     evento.currentTarget.disabled = true
 
     console.log(casillasOcupadas)
-    if(true === verificarGanador(casillasOcupadas))
-    {
-      console.log(`Tenemos un ganador: ${0 === nroTurno % 2? "X" : "O"}`)
-      setEsGanador(0 === nroTurno % 2? "X" : "O")
+    if (true === verificarGanador(casillasOcupadas)) {
+      console.log(`Tenemos un ganador: ${0 === nroTurno % 2 ? "X" : "O"}`)
+      setEsGanador(0 === nroTurno % 2 ? "X" : "O")
     }
 
     setNroTurno(nroTurno + 1)
   }
 
-  for(let i = 0; i < cantidadDeBotones; i++) 
-  {
+  for (let i = 0; i < cantidadDeBotones; i++) {
     botones.push(
       <Boton
-        key = {i} //Identificador para componentes en el árbol -> Reconciliación.
-        id = {`${i}`} //Identificar cada elemento del DOM
+        key={i} //Identificador para componentes en el árbol -> Reconciliación.
+        id={`${i}`} //Identificar cada elemento del DOM
         // value = {(i + 1).toString()}  //value = ""
-        value = ""
-        handleClick = {handleClick}
+        value=""
+        handleClick={handleClick}
       />
     )
   }
 
-  for (let i = 0; i < cantidadDeFilas; i++) 
-  {
+  for (let i = 0; i < cantidadDeFilas; i++) {
     filas.push(
-      <div className = "row" key={i}>
+      <div className="row" key={i}>
         {botones.slice(i * 3, (i + 1) * 3) /* shallow copy[copia referencia -> son componentes]: desde index 0, 3 y 6 hasta 3, 6 y 9 [sin incluir]*/}
       </div>
     )
   }
 
-  return(
+  return (
     <>
-      {filas}
-      {"" !== esGanador&& <span>Tenemos un ganador: {esGanador}</span>}
+      <div style={{ pointerEvents: esGanador !== "" ? 'none' : 'auto' }}>
+        {filas}
+      </div>
+      {"" !== esGanador && <span>Tenemos un ganador: {esGanador}</span>}
     </>
   )
 }
